d384e5cc8df4a60e8f66d354f7e72a75a68e4f3c
Lukas Stein <lukas.p.stein@gmail.com>
2024-01-11 17:59:06 +0100

    Quartz sync: Jan 11, 2024, 5:59 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "github.com/lukas-stein/lukas-stein",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
